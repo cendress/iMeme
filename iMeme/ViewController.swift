7d5003c384774505a23d099d99bf2c69ca890aef
cendress <95492688+cendress@users.noreply.github.com>
2024-01-09 14:10:31 -0500

    Create method for alert

--- iMeme/ViewController.swift
+++ iMeme/ViewController.swift
@@ -12,8 +12,6 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    view.backgroundColor = .brown
-    
     navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
     
     self.title = "iMeme"
@@ -35,10 +33,26 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
   //MARK: - UIImagePicker delegate methods
   
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//    if let image = info[.editedImage] as? UIImage {
-//      //do something with the edited image
-//    }
+    if let image = info[.editedImage] as? UIImage {
+      //do something
+    }
     
     dismiss(animated: true)
+  }
+  
+  //MARK: - Other methods
+  
+  private func showFirstText() {
+    let ac = UIAlertController(title: "Enter Some Text", message: "Enter some text for the meme", preferredStyle: .alert)
+    
+    ac.addTextField { textField in
+      textField.placeholder = "Enter text"
+    }
+    
+    ac.addAction(UIAlertAction(title: "Done", style: .default) { [weak self, ac] action in
+      //do something once you get the text
+    })
+    
+    present(ac, animated: true)
   }
 }
